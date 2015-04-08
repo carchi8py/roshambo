b53b90754e6f297743d208845a87dcfef9ed585e
Chris Archibald <carchi8py@gmail.com>
2015-04-07 20:25:27 -0700

    Creation of our 2 views

---
+++ roshambo/FightViewControler.swift
@@ -0,0 +1,14 @@
+//
+//  FightViewControler.swift
+//  roshambo
+//
+//  Created by Chris Archibald on 4/7/15.
+//  Copyright (c) 2015 Chris Archibald. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FightViewControler: UIViewController {
+    
+}
